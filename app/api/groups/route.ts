997aba6880064241d5abc856dee99bd1f44f1163
Shmookoff <shmookoff@gmail.com>
2023-10-23 18:14:42 +0500

    do not cache

--- app/api/groups/route.ts
+++ app/api/groups/route.ts
@@ -3,10 +3,12 @@ import { NextResponse } from 'next/server';
 import { studentPortalApi } from '@/server/1c';
 import { Fetch1CError } from '@/server/1c/error-schema';
 
+export const dynamic = 'force-dynamic';
+
 export const GET = async () => {
   try {
     const response = await studentPortalApi.groups.list();
-    return NextResponse.json(response)
+    return NextResponse.json(response);
   } catch (error) {
     if (error instanceof Fetch1CError)
       return NextResponse.json(error.text, { status: error.response.status });
