5a77db3f0622d336e94c9b5e3898e7c7171c44a7
Shmookoff <shmookoff@gmail.com>
2023-10-23 18:13:48 +0500

    refresh if token will not be valid in next 10 seconds

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,10 +1,27 @@
+import jwtDecode from 'jwt-decode';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { getTokenCookieOptions } from '@/lib/jwt';
 
 import { studentPortalApi } from '../1c';
 
-const skipUrls = ['/api', '/_next', '/favicon.ico', '/login'];
+const skipUrls = ['/_next', '/favicon.ico', '/login'];
+
+const refresh = async (refreshToken: string, response: NextResponse) => {
+  const refreshResponse = await studentPortalApi.auth.refresh(refreshToken);
+
+  response.cookies.set(
+    'AccessToken',
+    refreshResponse.access_token,
+    getTokenCookieOptions(refreshResponse.access_token)
+  );
+  response.cookies.set(
+    'RefreshToken',
+    refreshResponse.refresh_token,
+    getTokenCookieOptions(refreshResponse.refresh_token)
+  );
+  return response;
+};
 
 const authMiddleware = async (request: NextRequest, response: NextResponse) => {
   const skipUrlsMatches = skipUrls.map((url) =>
@@ -13,26 +30,17 @@ const authMiddleware = async (request: NextRequest, response: NextResponse) => {
   if (skipUrlsMatches.some((v) => v === true)) return;
 
   const accessToken = request.cookies.get('AccessToken');
-  if (accessToken) return;
-
   const refreshToken = request.cookies.get('RefreshToken');
-  if (!refreshToken) return;
 
-  try {
-    const refreshResponse = await studentPortalApi.auth.refresh(
-      refreshToken.value
-    );
-    response.cookies.set(
-      'AccessToken',
-      refreshResponse.access_token,
-      getTokenCookieOptions(refreshResponse.access_token)
-    );
-    response.cookies.set(
-      'RefreshToken',
-      refreshResponse.refresh_token,
-      getTokenCookieOptions(refreshResponse.refresh_token)
-    );
-  } catch {}
+  if (accessToken && refreshToken) {
+    const current = new Date();
+    const decodedAccess = jwtDecode<{ exp: number }>(accessToken.value);
+    if (decodedAccess.exp * 1000 - current.getTime() < 10000)
+      return await refresh(refreshToken.value, response);
+  }
+
+  if (!accessToken && refreshToken)
+    return await refresh(refreshToken.value, response);
 
   return response;
 };
