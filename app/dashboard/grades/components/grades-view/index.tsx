b06c78778de48213baf2fd617420cda32ff71776
shmookoff <shmookoff@gmail.com>
2023-10-27 15:51:09 +0500

    take today as default date

--- app/dashboard/grades/components/grades-view/index.tsx
+++ app/dashboard/grades/components/grades-view/index.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react';
 import GradesViewOnClient from './client';
 
 export const getDefaultOptions = async () => {
-  const to = new Date('2023-10-06');
+  const to = new Date();
   const from = subDays(to, 7);
   return { range: { from, to } };
 };
